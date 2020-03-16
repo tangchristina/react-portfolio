14e60f6e389b81f5fc89de29ea83273bb722dc33
Christina Tang <christinatangerine@hotmail.com>
2020-03-15 23:11:12 -0700

    about me page now renders

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,22 +1,24 @@
 import React from "react";
+import Jumbotron from "../components/Jumbotron"
+import CatImage from "../assets/images/cat.jpg"
 
 function About () {
     return (
-        <div className="jumbotron">
+        <Jumbotron>
         <section className="main-section">
           <h1>About Me</h1>
     
-          <img src="assets/images/cat.jpg" className="auth-image" alt="Your Name">
+          <img src={CatImage} className="auth-image" alt="Your Name"></img>
     
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum luctus felis, ac rhoncus nunc porttitor condimentum. Mauris sit amet dolor eu elit varius varius facilisis id arcu. Proin vel dui massa. Nunc rhoncus fringilla pharetra. Vestibulum bibendum consectetur euismod. Proin commodo purus vitae sollicitudin consectetur.</p>
     
           <p>Donec vehicula lacus at leo feugiat lacinia. In et ex ultrices, ultrices neque non, pretium odio. Nunc id eros iaculis, pulvinar quam eget, egestas mauris.</p>
     
           <p>Suspendisse dictum, arcu at convallis faucibus, quam sapien tincidunt nunc, sit amet egestas nibh tortor et mi. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
-        </img>
+        
         </section>
     
-      </div>
+    </Jumbotron>
     
     )
 }
