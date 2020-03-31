d2fa20b1e7e94e9d98b9b84dc0b7fa6e40374164
Christina Tang <christinatangerine@hotmail.com>
2020-03-30 21:51:24 -0700

    finished adding images, need to style

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron"
-import ClickyGameImage from "../assets/images/technics-q-c-640-480-1.jpg"
+import PsychicGameImage from "../assets/images/technics-q-c-640-480-1.jpg"
 import RPGGameImage from "../assets/images/technics-q-c-640-480-2.jpg"
-
+import TriviaGameImage from "../assets/images/technics-q-c-640-480-5.jpg"
+import uTrippinImage from "../assets/images/technics-q-c-640-480-7.jpg"
+import trainSchedulerImage from "../assets/images/technics-q-c-640-480-9.jpg"
+import FriendFinderImage from "../assets/images/friend-finder.jpg"
+import BurgerImage from "../assets/images/images.jpeg"
+import HotelImage from "../assets/images/hotel.jpeg"
+import ClickyGameImage from "../assets/images/bmw.jpeg"
+import GoogleBooksImage from "../assets/images/google-books.jpg"
 
 function Portfolio () {
     return (
@@ -20,7 +27,7 @@ function Portfolio () {
             
 
           <div className="work">
-            <a href="https://tangchristina.github.io/Psychic-Game/"><img src= {ClickyGameImage} alt="Psychic Game"></img></a>
+            <a href="https://tangchristina.github.io/Psychic-Game/"><img src= {PsychicGameImage} alt="Psychic Game"></img></a>
             <h3>Psychic Game</h3>
           </div>
          
@@ -31,44 +38,44 @@ function Portfolio () {
           </div>
       
           <div className="work">
-            <a href="https://tangchristina.github.io/TriviaGame/"><img src="assets/images/technics-q-c-640-480-5.jpg" alt="Trivia Game"></img></a>
+            <a href="https://tangchristina.github.io/TriviaGame/"><img src={TriviaGameImage} alt="Trivia Game"></img></a>
     
             <h3>Trivia Game</h3>
           </div>
           <div className="work">
-            <a href="https://eawhite00.github.io/UWBootCampProject1/"><img src="assets/images/technics-q-c-640-480-7.jpg" alt="Rutgers Info Widget"></img></a>
+            <a href="https://eawhite00.github.io/UWBootCampProject1/"><img src={uTrippinImage} alt="U Trippin"></img></a>
     
             <h3>uTrippin'</h3>
           </div>
 
           <div className="work">
-            <a href="https://tangchristina.github.io/train-scheduler/"><img src="assets/images/technics-q-c-640-480-9.jpg" alt="Rock Paper Scissors"></img></a>
+            <a href="https://tangchristina.github.io/train-scheduler/"><img src={trainSchedulerImage} alt="Train Scheduler"></img></a>
     
             <h3>Train Scheduler</h3>
         </div>
     
         <div className="work">
-          <a href="https://calm-mountain-13489.herokuapp.com/"><img src="assets/images/friend-finder.jpg" alt="Friend Finder"></img></a>
+          <a href="https://calm-mountain-13489.herokuapp.com/"><img src={FriendFinderImage} alt="Friend Finder"></img></a>
     
           <h3>Friend Finder</h3>
       </div>
       <div className="work">
-        <a href="https://serene-everglades-87109.herokuapp.com/"><img src="assets/images/images.jpeg" alt="Burger"></img></a>
+        <a href="https://serene-everglades-87109.herokuapp.com/"><img src={BurgerImage} alt="Burger"></img></a>
     
         <h3>Eat-Da-Burger</h3>
     </div>
     <div className="work">
-      <a href="https://shrouded-fortress-55458.herokuapp.com/"><img src="assets/images/hotel.jpeg" alt="Hotel"></img></a>
+      <a href="https://shrouded-fortress-55458.herokuapp.com/"><img src={HotelImage} alt="Hotel"></img></a>
     
       <h3>Hot Hotel</h3>
     </div>
     <div className="work">
-      <a href="https://tangchristina.github.io/clicky-game/"><img src="assets/images/bmw.jpeg" alt="clicky-game"></img></a>
+      <a href="https://tangchristina.github.io/clicky-game/"><img src={ClickyGameImage} alt="clicky-game"></img></a>
     
       <h3>Car Clicky Game</h3>
     </div>
     <div className="work">
-      <a href="http://lit-cliffs-37719.herokuapp.com/"><img src="assets/images/bmw.jpeg" alt="google-books"></img></a>
+      <a href="http://lit-cliffs-37719.herokuapp.com/"><img src={GoogleBooksImage} alt="google-books"></img></a>
     
       <h3>Google Books</h3>
     </div>
